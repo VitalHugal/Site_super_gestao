4352135c907ccbcd3508147ce74edd7e973a06cb
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-11 14:24:02 -0300

    .

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/',[PrincipalController::class,'principal']);
+Route::get('/contato',[ContatoController::class,'contato']);
+Route::get('/sobre-nos',[SobreNosController::class,'sobreNos']);
+
