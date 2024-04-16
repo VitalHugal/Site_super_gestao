c5870ec002d08365a708b3a421bcb61f3cd3901f
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-04-16 14:55:21 -0300

    .

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,23 +8,29 @@ use \App\Models\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-        // echo'<prev>';
-        // print_r($request->all());
-        // echo'</prev>';
-        // echo '<br>';
-        // echo '<br>';
-        // echo $request->input('nome');
-        // echo '<br>';
-        // echo $request->input('email');
 
+        // //1º metodo para armazenamento
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // $contato->save();
+
+
+
+        // //2º metodo para armazenamento
+        // $contato = new SiteContato();
+        // $contato->fill($request->all());
+        // $contato->save();
+
+
+
+        //3º metodo para armazenamento
         $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
+        $contato->create($request->all());
 
-        $contato->save();
         return view('site.contato');
     }
 }
