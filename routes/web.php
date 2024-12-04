9587b00f693f40ec77b9ea57065c115c94fc9301
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-12-04 18:44:51 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,9 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     
     Route::resource('cliente', ClienteController::class);
     Route::resource('pedido', PedidoController::class);
-    Route::resource('pedido-produto', PedidoProdutoController::class);
+    // Route::resource('pedido-produto', PedidoProdutoController::class);
+    Route::get('pedido-produto/create/{pedido}', [PedidoProdutoController::class, 'create'])->name('pedido-produto.create');
+    Route::post('pedido-produto/store/{pedido}', [PedidoProdutoController::class, 'store'])->name('pedido-produto.store');
 });
 
 //login
